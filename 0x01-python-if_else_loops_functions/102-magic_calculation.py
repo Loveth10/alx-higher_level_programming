0a1ddb770f15019f4e213d96275bfbd19bc9abe0
loveth10 <lovethokebe@gmail.com>
2022-08-25 04:51:45 +0000

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 15 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        else:
-            print(i, end=" ")
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return a * b - c
